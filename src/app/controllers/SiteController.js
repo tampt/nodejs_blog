cb0f8a938ab17a19837ff59a3415bf371b694361
tampt <tampt@nitrotech.asia>
2023-12-05 15:24:25 +0700

    add: add router and controller

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+
+class SiteController {
+
+    // [GET], /
+    index(req, res) {
+        res.render('home');
+    }
+
+    // [GET] /news/search
+    search(req, res) {
+        res.render('search');
+    }
+}
+
+module.exports = new SiteController;
--- src/index.js
+++ src/index.js
@@ -8,11 +8,18 @@ const app = express();
 
 const port = 3000;
 
+const route = require('./routes');
+
 // Hiển thị hình ảnh
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP Loggers
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine 
 app.engine('hbs', engine({
@@ -21,14 +28,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

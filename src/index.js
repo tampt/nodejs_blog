c122000fda8bdc8404a58233cfe4943c8e2f2ed9
tampt <tampt@nitrotech.asia>
2023-12-06 15:56:44 +0700

    message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 //const handlebars = require('express-handlebars');
-const { engine } = require ('express-handlebars');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
@@ -13,18 +13,23 @@ const route = require('./routes');
 // Hiển thị hình ảnh
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // HTTP Loggers
 // app.use(morgan('combined'));
 
-// Template engine 
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+// Template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -32,5 +37,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
